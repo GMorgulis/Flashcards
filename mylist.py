247ae026bc2675eb1f5589b6d22a12f9eecf9036
George Morgulis <gm3138@columbia.edu>
2025-03-28 16:15:09 -0400

    removed extra comments

--- mylist.py
+++ mylist.py
@@ -6,7 +6,7 @@ class Node:
         self.val1 = val1
         self.val2 = val2
         self.next = None
-        self.prev = None  # Add previous pointer
+        self.prev = None  
 
 class CircularLinkedList:
     def __init__(self):
@@ -16,28 +16,26 @@ class CircularLinkedList:
         new_node = Node(val1, val2)
         if not self.head:
             self.head = new_node
-            new_node.next = self.head  # Points to itself (circular)
-            new_node.prev = self.head  # Points to itself (circular)
+            new_node.next = self.head  
+            new_node.prev = self.head  
         else:
-            current = self.head.prev  # Last node (because it's circular)
+            current = self.head.prev 
             current.next = new_node
             new_node.prev = current
             new_node.next = self.head
-            self.head.prev = new_node  # Update head's previous pointer
+            self.head.prev = new_node  
 
 def create_list(filename):
-    # Create an instance of the CircularLinkedList
     my_list = CircularLinkedList()
     
     with open(filename, mode='r') as file:
         reader = csv.reader(file)
         
-        # Loop through each row in the CSV
         for row in reader:
             if len(row) != 2:
                 continue  # Skip rows that don't have exactly 2 values
             val1 = row[0]
             val2 = row[1]
-            my_list.add_node(val1, val2)  # Already supports prev pointer
+            my_list.add_node(val1, val2) 
     
     return my_list
